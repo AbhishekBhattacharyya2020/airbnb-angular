343e088f398fdcfd7b0e114c2cebf76e3a9ff0ff
AbhishekBhattacharyya2020 <66897732+AbhishekBhattacharyya2020@users.noreply.github.com>
2020-11-23 17:35:44 +0530

    Adds header component and scss styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  componentTitle = 'i AM A COMPONENT';
+  clickHandler(){
+    alert("I am clicked")
+  }
 }
